33888a145350267147e67d905b29c4e03b1eef76
= <=lucasferreiragalvaoti@gmail.com>
2019-03-04 10:58:42 -0300

    Admin - Categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use\Slim\Slim;
 use Devlfg\Page\Page;
 use Devlfg\Page\PageAdmin;
 use Devlfg\Model\User;
+use Devlfg\Model\Category;
 
 $app = new Slim();
 
@@ -176,6 +177,81 @@ $app->post("/admin/forgot/reset", function(){
 	
 
 	$page->setTpl("forgot-reset-success");
+});
+
+$app->get("/admin/categories",function(){
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories",[
+		"categories" => $categories
+	]);
+});
+
+$app->get("/admin/categories/create",function(){
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create",function(){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit();
+});
+
+$app->get("/admin/categories/:idcategory/delete",function($idcategory){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit();
+});
+
+$app->get("/admin/categories/:idcategory",function($idcategory){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update",[
+		"category" => $category->getValues()
+	]);
+});
+
+$app->post("/admin/categories/:idcategory",function($idcategory){
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit();
 });
 
 $app->run();
