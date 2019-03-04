dc790d0b90db545d85a72300c727828cb9b12dfc
= <=lucasferreiragalvaoti@gmail.com>
2019-03-04 15:20:20 -0300

    Admin Products

---
+++ site.php
@@ -0,0 +1,28 @@
+<?php
+
+    use \Devlfg\Page;
+    use \Devlfg\Model\Category;
+
+    $app->get('/', function() {
+	
+        $page = new Page();
+        $page->setTpl("index");
+    
+    });
+
+    $app->get("/categories/:idcategory",function($idcategory){
+    
+        $category = new Category();
+    
+        $category->get((int)$idcategory);
+    
+        $page = new Page();
+    
+        $page->setTpl("category",[
+            "category" => $category->getValues(),
+            "products" => []
+        ]);
+    
+    
+    });
+?>
