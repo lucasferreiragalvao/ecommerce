6dcb4b4d6e9539511e8f867c14ec3c293a3ba9c7
= <=lucasferreiragalvaoti@gmail.com>
2019-03-04 11:35:47 -0300

    Site Categorias

--- vendor/dev-lfg/php-classes/src/Model/Category.php
+++ vendor/dev-lfg/php-classes/src/Model/Category.php
@@ -24,6 +24,8 @@
             ));
 
             $this->setData($result[0]);
+
+            Category::updateFile();
         }
         public function get($idcategory){
             $sql = new Sql();
@@ -42,6 +44,23 @@
              WHERE idcategory = :idcategory",[
                  ":idcategory" => $this->getidcategory()
              ]);
+             
+             Category::updateFile();
+        }
+        public static function updateFile(){
+            $categories = Category::listAll();
+            
+            $html = [];
+
+            foreach($categories as $row){
+                array_push($html,'<li><a href="/categories/'.$row['idcategory'].'">'
+                .$row['descategory']
+                .'</a></li>');
+            }
+
+            file_put_contents($_SERVER['DOCUMENT_ROOT']. DIRECTORY_SEPARATOR.
+            "views" . DIRECTORY_SEPARATOR . "categories-menu.html", 
+            implode('',$html));
         }
        
     }
