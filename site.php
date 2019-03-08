23123852df146c527737a985c6796ed47e82a0f8
= <=lucasferreiragalvaoti@gmail.com>
2019-03-07 22:38:07 -0300

    Calculando Frete

--- site.php
+++ site.php
@@ -67,7 +67,8 @@
 
         $page->setTpl("cart",[
             "cart" => $cart->getValues(),
-            "products" => $cart->getProducts()
+            "products" => $cart->getProducts(),
+            "error" => Cart::getMsgError()
         ]);
     });
 
@@ -84,6 +85,16 @@
         for($i=0; $i < $qtd; $i++){
             $cart->addProduct($product);
         }
+
+        header("Location: /cart");
+        exit;
+    });
+
+    $app->post("/cart/freight" , function(){
+
+        $cart = Cart::getFromSession();
+
+        $cart->setFreight($_POST['zipcode']);
 
         header("Location: /cart");
         exit;
