f4296b55e516ea08769c1cef26d9f5a1b1c6e8cd
= <=lucasferreiragalvaoti@gmail.com>
2019-03-14 22:35:35 -0300

    Admin Pedidos

--- vendor/dev-lfg/php-classes/src/Model/Order.php
+++ vendor/dev-lfg/php-classes/src/Model/Order.php
@@ -3,8 +3,12 @@
 
     use \Devlfg\DB\Sql;
     use \Devlfg\Model;
+    use \Devlfg\Model\Cart;
 
     class Order extends Model{
+
+        const SESSION_ERROR = "OrderError";
+        const SUCCESS = "OrderSuccess";
 
         public function save(){
 
@@ -58,6 +62,79 @@
             if(count($results) > 0){
                 $this->setData($results[0]);
             }
+        }
+
+        public static function listAll(){
+
+            $sql = new Sql();
+
+            return $sql->select("
+                SELECT *
+                FROM tb_orders O
+                INNER JOIN tb_ordersstatus OS USING(idstatus)
+                INNER JOIN tb_carts C USING(idcart)
+                INNER JOIN tb_users U 
+                ON U.iduser = O.iduser
+                INNER JOIN tb_addresses A USING(idaddress)
+                INNER JOIN tb_persons P
+                ON P.idperson = U.idperson
+                ORDER BY O.dtregister DESC
+            ");
+
+
+        }
+
+        public function delete(){
+
+            $sql = new Sql();
+
+            $sql->query("DELETE FROM tb_orders WHERE idorder = :idorder",[
+                ":idorder" => $this->getidorder()
+            ]);
+        }
+
+        public function getCart():Cart
+        {
+
+            $cart = new Cart();
+
+            $cart->get((int)$this->getidcart());
+
+            return $cart;
+        }
+
+        public static function setError($msg){
+            $_SESSION[User::SESSION_ERROR] = $msg;
+        }
+ 
+        public static function getError(){
+            $msg = (isset($_SESSION[User::SESSION_ERROR]))
+            ? $_SESSION[Order::SESSION_ERROR] : "";
+ 
+            Order::clearError();
+ 
+            return $msg;
+        }
+        public static function clearError(){
+ 
+            $_SESSION[Order::SESSION_ERROR] = NULL;
+        }
+
+        public static function setSuccess($msg){
+            $_SESSION[Order::SUCCESS] = $msg;
+        }
+ 
+        public static function getSuccess(){
+            $msg = (isset($_SESSION[Order::SUCCESS]))
+            ? $_SESSION[Order::SUCCESS] : "";
+ 
+            Order::clearSuccess();
+ 
+            return $msg;
+        }
+        public static function clearSuccess(){
+ 
+            $_SESSION[Order::SUCCESS] = NULL;
         }
     }
 ?>
