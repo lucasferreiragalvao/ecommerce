2712f74eaa28000526766252f12affe068515860
= <=lucasferreiragalvaoti@gmail.com>
2019-03-17 03:09:18 -0300

    Ajustes Tela Usuário

--- admin-users.php
+++ admin-users.php
@@ -3,6 +3,55 @@
     use \Devlfg\Page\PageAdmin;
     use \Devlfg\Model\User;
 
+    $app->get('/admin/users/:iduser/password',function($iduser){
+        User::verifyLogin();
+
+        $user = new User();
+
+        $user->get((int)$iduser);
+
+        $page = new PageAdmin();
+        $page->setTpl("users-password", array(
+            "user" => $user->getValues(),
+            "msgError" => User::getError(),
+            "msgSuccess" => User::getSuccess()
+        ));
+
+    });
+
+    $app->post('/admin/users/:iduser/password',function($iduser){
+        User::verifyLogin();
+
+        if(!isset($_POST['despassword']) || $_POST['despassword'] === ''){
+
+            User::setError("Preencha a nova senha");
+            header("Location: /admin/users/$iduser/password");
+            exit;
+        }
+
+        if(!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm'] === ''){
+
+            User::setError("Preencha a confirmação da nova senha");
+            header("Location: /admin/users/$iduser/password");
+            exit;
+        }
+
+        if($_POST['despassword'] !== $_POST['despassword-confirm']){
+            User::setError("As senhas não conferem");
+            header("Location: /admin/users/$iduser/password");
+            exit;
+        }
+        $user = new User();
+
+        $user->get((int)$iduser);
+
+        $user->setPassword(User::getPasswordHash($_POST['despassword']));
+
+        User::setSuccess("Senha Alterada com sucesso!");
+        header("Location: /admin/users/$iduser/password");
+        exit;
+    });
+
     $app->get('/admin/users', function(){
         User::verifyLogin();
 
--- views-cache/categories-menu.b06c2a4417aa34d4a16ad8c7297f1e32.rtpl.php
+++
@@ -1,1 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/5">Android</a></li><li><a href="/categories/3">Apple</a></li><li><a href="/categories/4">Eletrônicos</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/7">Samsung</a></li>
